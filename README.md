c54beaaa3b6bb2d40513e53dc3a41bce2d178052
Graham Allen <grahamtallen94@gmail.com>
2020-09-30 04:30:23 -0700

    adds readme

---
+++ README.md
@@ -0,0 +1,5 @@
+A lamda that gets folding and project data from running FAH clients
+
+http://foldingathome.org
+
+https://github.com/FoldingAtHome/fah-web-client/blob/master/src/fah/webclient/js/main.js
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const axios = require("axios");
 
-async () =>  {
-  const url = "http://localhost:7396";
+
+(async () => {
+
+    const url = "http://localhost:7396";
   
-  const result = axios.put(url + '/api/session?_=' + Math.random());
+    const result = await axios.put(`${url}/api/session?_=${Math.random()}`);
+    const { data: sid } = result;
+    console.log({sid})
 
-}()
+    const { data: updatesData} = await axios.post(`${url}/api/updates?sid=${sid}`);
+    console.log({updatesData});
+    
+})()
 
-console.log({axios});
+
 
 
 
